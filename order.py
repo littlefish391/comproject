634d4d847ff012a549115e063d274a02648777d9
Daniel Rickerby <DanRickerby@users.noreply.github.com>
2018-08-16 11:39:40 +1200

    Update order.py
    Formatted menu

--- order.py
+++ order.py
@@ -1,4 +1,20 @@
-menu = {1 : 'Chips', 2 : 'Battered Fish', 3 : 'Crumbed Fish', 4 : 'Fillet', 5 : 'Hot Dog', 6 : 'Sausage', 7 : 'Homemade Meat Patty', 8 : 'Crabstick', 9 : 'Homemade Spring roll', 10 : 'Homemade Curry roll', 11 : 'Homemade potato fritter', 12 : 'Homemade Paua Fritter', 13 : 'Chicken Nugget', 14 : 'Mini Hot Dog', 15 : 'Pineapple Firtter'}
-menu_prices = {menu[1] : '2.80', menu[2] : '2.90'}
+menu = {1 : 'Chips', 
+        2 : 'Battered Fish',
+        3 : 'Crumbed Fish',
+        4 : 'Fillet',
+        5 : 'Hot Dog',
+        6 : 'Sausage',
+        7 : 'Homemade Meat Patty',
+        8 : 'Crabstick',
+        9 : 'Homemade Spring Roll',
+        10 : 'Homemade Curry Roll',
+        11 : 'Homemade Potato Fritter',
+        12 : 'Homemade Paua Fritter',
+        13 : 'Chicken Nugget',
+        14 : 'Mini Hot Dog',
+        15 : 'Pineapple Fritter'}
+
+menu_prices = {menu[1] : '2.80',
+               menu[2] : '2.90'}
 
 print(menu_prices)
