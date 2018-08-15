f750aa069ed737c82cdb38fee7f44ae60d90f236
littlefish391 <littlefish391@gmail.com>
2018-08-16 11:34:36 +1200

    Add files via upload

---
+++ order.py
@@ -0,0 +1,4 @@
+menu = {1 : 'Chips', 2 : 'Battered Fish', 3 : 'Crumbed Fish', 4 : 'Fillet', 5 : 'Hot Dog', 6 : 'Sausage', 7 : 'Homemade Meat Patty', 8 : 'Crabstick', 9 : 'Homemade Spring roll', 10 : 'Homemade Curry roll', 11 : 'Homemade potato fritter', 12 : 'Homemade Paua Fritter', 13 : 'Chicken Nugget', 14 : 'Mini Hot Dog', 15 : 'Pineapple Firtter'}
+menu_prices = {menu[1] : '2.80', menu[2] : '2.90'}
+
+print(menu_prices)
